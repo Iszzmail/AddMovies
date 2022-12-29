65d7d8c98fa6d3e796c975c6ba4f59aa9832b965
Iszzmail <ismailmohammed200@gmail.com>
2022-12-29 20:17:34 +0530

    push

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -9,53 +9,47 @@ let del = ''
 
 let getMovieDetails = () => {
     let movieTitle = document.getElementsByName('title')[0].value
-
-    fetch('https://api.unsplash.com/search/photos?page=1&query=office&client_id=ZGEqVh2wMD8_NVIUH1L2HGSMRHq9opBg2ohslkN7L9k')
-        .then(response => response.json())
-        .then(data => {
-            let imageUrl = data.results[0].url.raw;
-            console.log(imageUrl);
-        })
-        .catch(error => console.error(error));
-
-
-    let ImageURL = ''
     let YourRating = document.getElementsByName('rating')[0].value
-    let newMovieElement = document.createElement('li')
-    newMovieElement.className = 'movie-element'
-    newMovieElement.innerHTML = `
-    <div class="movie-element__image"
-    > <img id='img' src="${ImageURL}" alt=${movieTitle}>
-    </div> 
-    <div class="movie-element__info">
-    <h2>${movieTitle}</h2>
-    <p>${YourRating}/5 stars</p>
-    `
-    showMovies.append(newMovieElement)
-    const addButton = document.createElement('button')
-    addButton.innerHTML = "Delete"
-    addButton.className = "deleting"
-    addButton.value = Math.random()
-    newMovieElement.append(addButton)
 
-    addButton.addEventListener("click", (e) => {
-        let val = e.target.value
-        console.log(val)
-        let a = 0
-        for (let i = 0; i < showMovies.children.length; i++) {
-            if (val === showMovies.children[i].querySelector('.deleting').value)
-                showMovies.children[i].remove()
-            a = i
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${movieTitle}&client_id=ZGEqVh2wMD8_NVIUH1L2HGSMRHq9opBg2ohslkN7L9k`)
+        .then(response => response.json())
+        .then((a) => {
+            let ImageURL = a.results[0].urls.small
+            let newMovieElement = document.createElement('li')
+            newMovieElement.className = 'movie-element'
+            newMovieElement.innerHTML = `
+            <div class="movie-element__image"
+            > <img id='img' src="${ImageURL}" alt=${movieTitle}>
+            </div> 
+            <div class="movie-element__info">
+            <h2>${movieTitle}</h2>
+            <p>${YourRating}/5 stars</p>
+            `
+            showMovies.append(newMovieElement)
+            const addButton = document.createElement('button')
+            addButton.innerHTML = "Delete"
+            addButton.className = "deleting"
+            addButton.value = Math.random()
+            newMovieElement.append(addButton)
+            addButton.addEventListener("click", (e) => {
+                let val = e.target.value
+                console.log(val)
+                let a = 0
+                for (let i = 0; i < showMovies.children.length; i++) {
+                    if (val === showMovies.children[i].querySelector('.deleting').value)
+                        showMovies.children[i].remove()
+                    a = i
+                }
+                logs.splice(a, 1)
+            })
+            logInObj(Math.random(), movieTitle, YourRating)
+
+            console.log(logs)
+            cancelEntering()
         }
-        logs.splice(a, 1)
-    })
 
-
-
-    logInObj(Math.random(), movieTitle, ImageURL, YourRating)
-
-    console.log(logs)
-    cancelEntering()
+        )
+        .catch(error => console.error(error));
 }
 
 
@@ -65,11 +59,10 @@ let deleteMovies = () => {
 }
 
 
-let logInObj = (id, name, imgurl, rating) => {
+let logInObj = (id, name, rating) => {
     let objlog = {
         num: id,
         movieName: name,
-        image: imgurl,
         ratings: rating
     }
 
@@ -98,10 +91,13 @@ let toggleElement = () => {
 let cancelEntering = () => {
     model.style.display = "none"
     document.getElementsByName('title')[0].value = ''
-    document.getElementsByName('image-url')[0].value = ''
     document.getElementsByName('rating')[0].value = ''
 }
 
 buttons.addEventListener('click', toggleElement)
 addMoviesToList.addEventListener('click', getMovieDetails)
 cancel.addEventListener('click', cancelEntering)
+
+
+
+
